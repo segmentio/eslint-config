7701b01667b336fa6330415bc6b6bf8377a293f5
Nathan Houle <nathan@nathanhoule.com>
2015-05-18 18:59:29 -0700

    Add node-specific settings

--- node.js
+++ node.js
@@ -14,5 +14,11 @@ var merge = require('lodash.merge');
 module.exports = merge(base, {
   'env': {
     'node': true
+  },
+
+  'rules': {
+    'handle-callback-err': [1, '^(err|error)$'],
+    'no-path-concat': 2,
+    'no-sync': 1
   }
 });
