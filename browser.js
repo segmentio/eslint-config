00fdf4970e2af08f90559a0a32463310cee4f7e6
Dominic Barnes <dominic@dbarnes.info>
2015-06-08 19:40:16 +0000

    also disabling global-strict rule (like node env) for browser

--- browser.js
+++ browser.js
@@ -16,6 +16,7 @@ module.exports = merge({}, base, {
     browser: true
   },
   rules: {
+    'global-strict': [0, 'always'],
     'new-cap': [ 2, { capIsNewExceptions: [ 'Emitter', 'Enumerable', 'Validator' ] } ],
     // We frequently use console.log in development, and most of our libraries
     // will depend on linting success to run tests
