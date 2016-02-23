f9897f4b4a38211ceaddd48601c4f58ed45a5975
Nathan Houle <nathan@nathanhoule.com>
2016-02-22 17:23:40 -0800

    Add require-path-exists plugin to check requires

--- server/index.js
+++ server/index.js
@@ -12,9 +12,16 @@ module.exports = {
     "ecmaVersion": 6
   },
 
+  "plugins": [
+    "require-path-exists"
+  ],
+
   "rules": {
     "handle-callback-err": [1, "^(err|.+Err|error|.+Error)$"],
     "no-path-concat": 2,
-    "no-sync": 2
+    "no-sync": 2,
+    "require-path-exists/exists": 2,
+    "require-path-exists/notEmpty": 2,
+    "require-path-exists/tooManyArguments": 2
   }
 };
