14386507c2801840f6d7362a216558547ada12d2
Nathan Houle <nathan@nathanhoule.com>
2016-02-22 17:15:52 -0800

    Add eslint-plugin-mocha plugin

--- mocha/index.js
+++ mocha/index.js
@@ -5,8 +5,16 @@ module.exports = {
     "mocha": true
   },
 
+  "plugins": [
+    "mocha"
+  ],
+
   "rules": {
     // Deeply nested functions are a reality of Mocha+BDD, so permit them
-    "max-nested-callbacks": 0
+    "max-nested-callbacks": 0,
+    "mocha/handle-done-callback": 2,
+    "mocha/no-exclusive-tests": 2,
+    "mocha/no-global-tests": 2,
+    "require-jsdoc": 0
   }
 };
