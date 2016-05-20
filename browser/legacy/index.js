a16fb73a3968c503c6a28e2bf96b4d8503dcef57
Nathan Houle <nathan+github@nathanhoule.com>
2016-05-20 14:30:12 -0700

    Disallow use of global JSON in legacy environments (#58)

--- browser/legacy/index.js
+++ browser/legacy/index.js
@@ -2,7 +2,7 @@
 
 /**
  * Extends browser configuration with ES3 settings.
- * Use when targeting old browsers, e.g. IE 8/7/6.
+ * Use when targeting old browsers, e.g. IE7/8.
  */
 
 module.exports = {
@@ -16,6 +16,7 @@ module.exports = {
     "comma-dangle": ["error", "never"],
     "dot-notation": ["error", { "allowKeywords": false }],
     "no-catch-shadow": "error",
+    "no-restricted-globals": ["error", "JSON"], // IE7
     "quote-props": ["error", "as-needed", { "keywords": true }],
     "radix": ["error", "always"]
   }
