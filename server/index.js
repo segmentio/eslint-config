62180f75a604f93a4d3b6627e88d5077e4956e16
Chris Nixon <chris.nixon@segment.com>
2019-06-07 13:02:40 -0700

    Use Airbnb and Prettier for server config (#64)
    * Use airbnb and prettier for server-side linting
    
    * Release commit

--- server/index.js
+++ server/index.js
@@ -1,27 +1,6 @@
-'use strict';
-
 module.exports = {
-  "extends": "@segment/eslint-config",
-
-  "env": {
-    "es6": true,
-    "node": true
-  },
-
-  "parserOptions": {
-    "ecmaVersion": 6
-  },
-
-  "plugins": [
-    "require-path-exists"
-  ],
-
-  "rules": {
-    "handle-callback-err": ["warn", "^(err|.+Err|error|.+Error)$"],
-    "no-path-concat": "error",
-    "no-sync": "error",
-    "require-path-exists/exists": "error",
-    "require-path-exists/notEmpty": "error",
-    "require-path-exists/tooManyArguments": "error"
+  extends: ['airbnb-base', 'prettier'],
+  rules: {
+    'func-names': 0
   }
 };
